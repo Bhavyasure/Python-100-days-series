171aec6539e45127a4e163f642c32bfc080fdd0e
Bhavyasure <bhavyavarshithasure2005@gmail.com>
2025-06-08 07:50:28 +0530

    Add files via upload

---
+++ python 100 days/project1 BandName gen.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city=input("What's the name of the city you grew up in?\n")
+name=input("What's your nick name?\n")
+print("your band name could be "+ city +" "+ name )
